5caa905d1d98a4eea30939f1b8ecd03bde523394
unknown <gokalpcakar@gmail.com>
2021-11-25 19:05:55 +0300

    Startup'ta interface'le class ilişkilendirildi, veritabanı bağlantısı eklendi.

--- PrometheusAppWebApi/Startup.cs
+++ PrometheusAppWebApi/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PrometheusAppWebApi.Repository.Abstract;
+using PrometheusAppWebApi.Repository.Concrete;
+using PrometheusAppWebApi.Repository.Concrete.EntityFramework;
 
 namespace PrometheusAppWebApi
 {
@@ -25,8 +29,14 @@ namespace PrometheusAppWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // localhosttaki veritabaný baðlantýsý ekleniyor.
+            services.AddDbContext<UserDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
 
             services.AddControllers();
+
+            // interface ile sýnýf iliþkilendiriliyor.
+            services.AddScoped<IUserRepository, EfUserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
